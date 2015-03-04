bad5521ab2f3b08a00e00f26ee61c1d68730de56
Peter Garnæs <petergarnaes@gmail.com>
2015-03-04 18:18:13 +0100

    No need for using support library

--- app/src/main/java/android_app/gg/peter/madklub/calendar/CalendarFragment.java
+++ app/src/main/java/android_app/gg/peter/madklub/calendar/CalendarFragment.java
@@ -1,10 +1,10 @@
 package android_app.gg.peter.madklub.calendar;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
