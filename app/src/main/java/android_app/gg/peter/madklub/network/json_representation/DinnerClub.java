1fab478267d68fff338dc8ed8b5f61830baea8e7
Peter Garnæs <petergarnaes@gmail.com>
2015-03-04 16:15:29 +0100

    Put everything into DinnerClub, as it will soon only be used for parsing json, and list of participants will be synced along with dinnerclub. Id will also be parsed from json so we can keep ids synced with server

--- app/src/main/java/android_app/gg/peter/madklub/network/json_representation/DinnerClub.java
+++ app/src/main/java/android_app/gg/peter/madklub/network/json_representation/DinnerClub.java
@@ -1,17 +1,20 @@
 package android_app.gg.peter.madklub.network.json_representation;
 
 import java.util.Calendar;
+import java.util.List;
 
 /**
  * Created by peter on 2/21/15.
  */
 public class DinnerClub {
+    private int id;
     private Calendar date;
     private Course course;
     private Person cook;
     private boolean shopped;
     private boolean paticipating;
     private float price;
+    private List<Person> participants;
 
     public DinnerClub(Calendar date,String cook,String course) {
         this.date = date;
@@ -61,5 +64,8 @@ public class DinnerClub {
 
     public float getPrice() {
         return price;
+    }
+    public List<Person> getParticipants() {
+        return participants;
     }
 }
